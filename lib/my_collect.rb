ee424c92058f3a34c98815ea581f98d791299699
Craig Storer <craigstorer@gmail.com>
2020-03-10 17:43:04 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,12 @@
+require 'pry'
 
-
+def my_collect(array)
+  # binding.pry
+  collection = []
+  i = 0 
+  while i < array.length
+   collection <<  yield(array[i])
+   i = i + 1
+end
+collection
+end
